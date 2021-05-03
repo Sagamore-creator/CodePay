d7a2fba012a21fa96738b0fb5bc5c3d1e41a9875
Lech Lipnicki <lech.lipnicki@gmail.com>
2021-05-03 18:54:25 +0300

    Initial commit

---
+++ CodePay/CodePay/Scenes/Main/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  CodePay
+//
+//  Created by Lech Lipnicki on 2021-05-03.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
